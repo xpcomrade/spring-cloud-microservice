67e37a14dcd95f1920ef76045c3dc4f002054e9f
xpcomrade <xpcomrade@gmail.com>
2016-12-16 14:47:51 +0800

    Add files comments

--- service-consumer-examples/ribbon-consumer/src/main/java/com/xpcomrade/microservice/RibbonConsumerApplication.java
+++ service-consumer-examples/ribbon-consumer/src/main/java/com/xpcomrade/microservice/RibbonConsumerApplication.java
@@ -2,6 +2,7 @@ package com.xpcomrade.microservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
@@ -13,7 +14,8 @@ import org.springframework.web.client.RestTemplate;
  * Description: TODO(这里用一句话描述这个类的作用). <br/>
  */
 @SpringBootApplication
-@EnableDiscoveryClient //通过@EnableDiscoveryClient注解来添加发现服务能力
+@EnableDiscoveryClient //通过注解来添加发现服务能力
+@EnableCircuitBreaker // 通过注解开启断路器功能
 public class RibbonConsumerApplication {
 
     @Bean
